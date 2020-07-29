4ba1f83dfc7fad9fbfb916cadf85c056cc92cf21
Ezra Black <dev.ezrablack@gmail.com>
2020-07-29 18:09:20 -0500

    Generation Cycle functionality implemented

--- ConwaysGOL/ConwaysGOL/Grid.swift
+++ ConwaysGOL/ConwaysGOL/Grid.swift
@@ -21,10 +21,10 @@ class Grid: NSObject {
     let size: Int
     var cellsInGrid: [Cell] = []
     var delegate: GameStateDel?
-
+    
     var gen = 0
     var pop: Int { cellsInGrid.filter { $0.state == .alive }.count }
-
+    
     public init(gridSize: Int) {
         self.size = gridSize
         for y in 0..<size {
@@ -51,8 +51,62 @@ class Grid: NSObject {
         return cellsInGrid[position]
     }
     
-    private func callDelegate() {
+    func CellCoordinates(index: Int) -> (x: Int, y: Int) {
+        var x = 0
+        var y = 0
+        y = index / size
+        x = index - (y * size)
+        return (x, y)
+    }
+    
+    func cycleGeneration() {
+        var index = 0
+        var kill: [Cell] = []
+        var birth: [Cell] = []
         
+        for cell in cellsInGrid {
+            var count = 0
+            let coord = CellCoordinates(index: index)
+            //W
+            if coord.x != 0 { if cellAt(x: coord.x - 1, y: coord.y).state == .alive { count = count + 1 }}
+            //NW
+            if coord.x != 0 && coord.y != 0 { if cellAt(x: coord.x - 1, y: coord.y - 1).state == .alive { count = count + 1 }}
+            //N
+            if coord.y != 0 { if cellAt(x: coord.x, y: coord.y - 1).state == .alive {count = count + 1}}
+            //NE
+            if coord.x < (size - 1) && coord.y != 0 { if cellAt(x: coord.x + 1, y: coord.y - 1).state == .alive { count = count + 1 }}
+            //E
+            if coord.x < (size - 1) { if cellAt(x: coord.x + 1, y: coord.y).state == .alive { count = count + 1 }}
+            //SE
+            if coord.x < (size - 1) && coord.y < (size - 1) { if cellAt(x: coord.x + 1, y: coord.y + 1).state == .alive { count = count + 1 }}
+            // S
+            if coord.y < (size - 1) { if cellAt(x: coord.x, y: coord.y + 1).state == .alive { count = count + 1 }}
+            // SW
+            if coord.x != 0 && coord.y < (size - 1) { if cellAt(x: coord.x - 1, y: coord.y + 1).state == .alive { count = count + 1 }}
+            
+            if cell.state == .alive {
+                if count < 2 || count > 3 {
+                    kill.append(cell)
+                }
+            } else { // cell.state == .dead
+                if count == 3 {
+                    birth.append(cell)
+                }
+            }
+            index = index + 1
+        }
+        
+        for cell in kill { cell.state = .dead }
+        
+        for cell in birth { cell.state = .alive }
+        gen += 1
+        callDelegate()
+    }
+    
+    private func callDelegate() {
+        delegate?.presentGeneration()
+        delegate?.presentPopulation()
+        delegate?.updateGrid()
     }
 }
 
@@ -60,5 +114,6 @@ class Grid: NSObject {
 //game lifecycle
 
 
+  
 
 //game state functionality/ clearing screen/ updating patterns
