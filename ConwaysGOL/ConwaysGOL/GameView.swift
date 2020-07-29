735426ff0fe407b301e5eaf768d44e1a8683e9c2
Ezra Black <dev.ezrablack@gmail.com>
2020-07-29 17:50:28 -0500

    Updated View For Landscape

--- ConwaysGOL/ConwaysGOL/GameView.swift
+++ ConwaysGOL/ConwaysGOL/GameView.swift
@@ -11,5 +11,38 @@ import UIKit
 class GameView: UIView {
 
    //functionality for grid
+    //need grid object
+    //cells size for grid
+    //timer to check intervals
+    //the time speed and if it is running
+    
+    //making the grid with simple frame formats. CGRect?
+    
+    
+    
+    public func cancelTime() {
+        
+    }
 
+    //Cell Tapped?
+    
+    public func tappedCell(at index: Int) {
+        
+    }
+    
+    public func gridClear() {
+        
+    }
+    
+    public func timerStart() {
+        
+    }
+    //One iteration over a generation, step by step mode.
+    public func step() {
+        
+    }
+    //clears grid and implements basic shapes known to most devs in the community/famous shapes such as ships.
+    public func Patterns() {
+        
+    }
 }
--- ConwaysGOL/ConwaysGOL/Grid.swift
+++ ConwaysGOL/ConwaysGOL/Grid.swift
@@ -10,36 +10,55 @@ import Foundation
 
 // TODO: Game Stats/ Game Grid/ Grid Functionality/ Delegation
 protocol GameStateDel {
-    func generation()
-    func population()
+    func presentGeneration()
+    func presentPopulation()
     func updateGrid()
 }
 
 //game grid class
 
 class Grid: NSObject {
-//    let size: Int
-//    var cellsInGrid: [Cell] = []
-//    var delegate: GameStateDel?
-//
-//    var gen = 0
-//    var pop: Int {
-//        cellsInGrid.filter{ $0.state == .alive }.count
-//    }
+    let size: Int
+    var cellsInGrid: [Cell] = []
+    var delegate: GameStateDel?
 
-//    public init(gridSize: Int) {
-//        self.size = gridSize
-//        for y in 0..<size {
-//            for x in 0..<size {
-//                let cell = Cell(x: x, y: y)
-//                cellsInGrid.append(cell)
-//            }
-//        }
-//    }
+    var gen = 0
+    var pop: Int { cellsInGrid.filter { $0.state == .alive }.count }
+
+    public init(gridSize: Int) {
+        self.size = gridSize
+        for y in 0..<size {
+            for x in 0..<size {
+                let cell = Cell(x: x, y: y)
+                cellsInGrid.append(cell)
+            }
+        }
+        super.init()
+    }
+    
+    func cellTapped(at index: Int) {
+        if cellsInGrid[index].state == .alive {
+            cellsInGrid[index].state = .dead
+        } else { cellsInGrid[index].state = .alive
+            
+        }
+        callDelegate()
+    }
+    
+    func cellAt(x: Int, y: Int) -> Cell {
+        var position: Int
+        position = (y * size) + x
+        return cellsInGrid[position]
+    }
+    
+    private func callDelegate() {
+        
+    }
 }
 
 
-
 //game lifecycle
+
+
 
 //game state functionality/ clearing screen/ updating patterns
